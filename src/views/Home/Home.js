7d79800ef2eb4c4855e344f5c1c620e9015aeb06
smitapatare1 <smitapatare2002@gmail.com>
2022-12-10 17:09:07 +0530

    title coding completed

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,32 +1,34 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css"
 import NavBar from "./../../components/NavBar/NavBar";
 
-function Home(){
-    const [notes, setNotes]=useState([
+function Home() {
+    const [notes, setNotes] = useState([
 
-        { 
+        {
             title: "Note 1",
-            content : "this is the content of note 1"
+            content: "this is the content of note 1"
         },
-        { 
+        {
             title: "Note 2",
-            content : "this is the content of note 2"
+            content: "this is the content of note 2"
         },
-        { 
+        {
             title: "Note 3",
-            content : "this is the content of note 3"
+            content: "this is the content of note 3"
         },
-        { 
+        {
             title: "Note 4",
-            content : "this is the content of note 4"
+            content: "this is the content of note 4"
         }
     ]);
-    return(
-            <div>
-                <NavBar/>
-                <h1 className="app-title">my notes</h1>                
+    return (
+        <div>
+            <NavBar />
+            <div  className="app-title-container">
+                <h1  className="app-title">my notes</h1>
             </div>
+        </div>
     )
 }
 export default Home
