cc55d86295199e91ded73a721339d102b8f1bf1f
smitapatare1 <smitapatare2002@gmail.com>
2022-12-10 16:09:29 +0530

    import NavBar in Home.js

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Home.css"
+import NavBar from "./../../components/NavBar/NavBar";
 
 function Home(){
     return(
             <div>
+                <NavBar/>
                 <h1> home page</h1>
             </div>
     )
