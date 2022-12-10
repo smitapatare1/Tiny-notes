45c4afe0807b6620d877ddd43301870faf8d0232
smitapatare1 <smitapatare2002@gmail.com>
2022-12-10 15:39:47 +0530

    initial commit

---
+++ src/views/AddNote/AddNote.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AddNote.css"
+
+function AddNote(){
+    return(
+            <div>
+                <h1>this is AddNote page</h1>
+            </div>
+    )
+}
+export default AddNote
