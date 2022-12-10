f84b0cc18198e953c58c75e9c332c988e0653254
smitapatare1 <smitapatare2002@gmail.com>
2022-12-10 16:57:28 +0530

    array created in Home.js

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import "./Home.css"
 import NavBar from "./../../components/NavBar/NavBar";
 
 function Home(){
+    const [notes, setNotes]=useState([
+
+        { 
+            title: "Note 1",
+            content : "this is the content of note 1"
+        },
+        { 
+            title: "Note 2",
+            content : "this is the content of note 2"
+        },
+        { 
+            title: "Note 3",
+            content : "this is the content of note 3"
+        },
+        { 
+            title: "Note 4",
+            content : "this is the content of note 4"
+        }
+    ]);
     return(
             <div>
                 <NavBar/>
-                <h1> home page</h1>
+                <h1 className="app-title">all notes</h1>                
             </div>
     )
 }
