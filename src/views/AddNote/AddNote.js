90e201fbfe1b147d756a2190e006b8157eebdb4b
smitapatare1 <smitapatare2002@gmail.com>
2022-12-10 16:12:40 +0530

    navbar import & show in AddNote

--- src/views/AddNote/AddNote.js
+++ src/views/AddNote/AddNote.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./AddNote.css"
+import NavBar from "./../../components/NavBar/NavBar";
 
 function AddNote(){
     return(
             <div>
+                <NavBar/>
                 <h1>this is AddNote page</h1>
             </div>
     )
