1b4e730f6f7d88d9bd3a8fd0b7a7cc731237230d
smitapatare1 <smitapatare2002@gmail.com>
2022-12-10 15:42:45 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes , Route } from "react-router-dom"
 
+
 import "./App.css"
 import AddNote from "./views/AddNote/AddNote"
 import Home from "./views/Home/Home"
