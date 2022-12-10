f7a111c43e350b5d76dd3ed18b78fc60f28a2060
smitapatare1 <smitapatare2002@gmail.com>
2022-12-10 16:39:02 +0530

    use Link from react-router-dom

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 function NavBar(){
     return(
         <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">📑 Tiny-Notes  </a>
+    <Link class="navbar-brand" to="/">📑 Tiny-Notes  </Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,11 +14,11 @@ function NavBar(){
       <ul class="navbar-nav">
 
         <li class="nav-item">
-          <a class="nav-link " aria-current="page" href="#">Home</a>
+          <Link class="nav-link " aria-current="page" to="/">Home</Link>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link " aria-current="page" href="#">AddNote</a>
+          <Link class="nav-link " aria-current="page" to="/add-note">AddNote</Link>
         </li>
 
 
